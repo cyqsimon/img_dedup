aa76d17555009c83d167e15d951730e877e3e925
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2021-05-19 18:13:03 +0800

    Added clap def

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::{load_yaml, App};
+
 fn main() {
+    let clap_def = load_yaml!("cli_def.yaml");
+    let matches = App::from_yaml(clap_def).get_matches();
+
+    println!("{:?}", matches);
+
     img_kernel_convolution::hello_world();
 }
